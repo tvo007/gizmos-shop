8591dbe84789a4c67b8d5f57b0446f831f06463a
Tim Vo <timtvo714@gmail.com>
2022-04-29 21:33:47 -0700

    disclosure, fixed add to cart bug from product

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -6,6 +6,7 @@ import {SingleProduct} from '../../lib/queries';
 import useStore from '../../lib/store';
 import {v4 as uuidv4} from 'uuid';
 import Dinero from 'dinero.js';
+import {Disclosure} from '@headlessui/react';
 
 const Product = ({product}) => {
   const productHandle = product.handle;
@@ -25,7 +26,7 @@ const Product = ({product}) => {
       product_name: productName,
       price: price,
       quantity: '1',
-      product_img: image,
+      product_img: image.url,
       id: uuidv4 (),
     });
   };
@@ -117,44 +118,58 @@ const Product = ({product}) => {
                 <h4 className="mb-6 font-heading font-medium">
                   More information
                 </h4>
-                <button className="flex w-full pl-6 lg:pl-12 pr-6 py-4 mb-4 justify-between items-center leading-7 rounded-2xl border-2 border-blueGray-200 hover:border-blueGray-300">
-                  <h3 className="text-lg font-heading font-medium">
-                    Shipping &amp; returns
-                  </h3>
-                  <span>
-                    <svg
-                      width="12"
-                      height="8"
-                      viewBox="0 0 12 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z"
-                        fill="black"
-                      />
-                    </svg>
-                  </span>
-                </button>
-                <button className="flex w-full pl-6 lg:pl-12 pr-6 py-4 justify-between items-center leading-7 rounded-2xl border-2 border-blueGray-200 hover:border-blueGray-300">
-                  <h3 className="text-lg font-heading font-medium">
-                    Product details
-                  </h3>
-                  <span>
-                    <svg
-                      width="12"
-                      height="8"
-                      viewBox="0 0 12 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z"
-                        fill="black"
-                      />
-                    </svg>
-                  </span>
-                </button>
+                <Disclosure>
+                  <Disclosure.Button className="flex w-full pl-6 lg:pl-12 pr-6 py-4 mb-4 justify-between items-center leading-7 rounded-2xl border-2 border-blueGray-200 hover:border-blueGray-300">
+                    <h3 className="text-lg font-heading font-medium">
+                      Shipping &amp; returns
+                    </h3>
+                    <span>
+                      <svg
+                        width="12"
+                        height="8"
+                        viewBox="0 0 12 8"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z"
+                          fill="black"
+                        />
+                      </svg>
+                    </span>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="text-gray-500">
+                    <p className="pl-6 lg:pl-12 xl:pl-12 pr-[2rem] pb-[1rem]">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tempore sequi recusandae praesentium illum fugiat maxime, iure sint, in, sunt asperiores omnis maiores aperiam odit corporis provident doloribus veritatis magni!
+                    </p>
+                  </Disclosure.Panel>
+                </Disclosure>
+                <Disclosure>
+                  <Disclosure.Button className="flex w-full pl-6 lg:pl-12 pr-6 py-4 justify-between items-center leading-7 rounded-2xl border-2 border-blueGray-200 hover:border-blueGray-300">
+                    <h3 className="text-lg font-heading font-medium">
+                      Product details
+                    </h3>
+                    <span>
+                      <svg
+                        width="12"
+                        height="8"
+                        viewBox="0 0 12 8"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z"
+                          fill="black"
+                        />
+                      </svg>
+                    </span>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="text-gray-500">
+                    <p className="pl-6 lg:pl-12 xl:pl-12 pr-[2rem] py-[1rem]">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tempore sequi recusandae praesentium illum fugiat maxime, iure sint, in, sunt asperiores omnis maiores aperiam odit corporis provident doloribus veritatis magni!
+                    </p>
+                  </Disclosure.Panel>
+                </Disclosure>
               </div>
             </div>
           </div>

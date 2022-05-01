1c7308045c9f026d19e7f2497f7ec23f2cf531cb
Tim Vo <timtvo714@gmail.com>
2022-04-30 23:07:05 -0700

    cleanup

--- pages/cart.js
+++ pages/cart.js
@@ -12,10 +12,10 @@ const Cart = () => {
   const {tempOrder} = useStore ();
   // console.log (sumOrders (tempOrder));
   console.log (tempOrder);
-  const formattedShipping = Dinero ({
-    amount: shippingCost,
-    precision: 0,
-  }).toFormat ('$0,0.00');
+  // const formattedShipping = Dinero ({
+  //   amount: shippingCost,
+  //   precision: 0,
+  // }).toFormat ('$0,0.00');
 
   const [isLoading, setIsLoading] = useState (false);
 
@@ -47,49 +47,6 @@ const Cart = () => {
     <Layout>
       <section className="pt-12 pb-24 bg-blueGray-100">
         <div className="container px-4 mx-auto">
-          <ul className="flex flex-wrap items-center mb-10 xl:mb-0">
-            <li className="mr-6">
-              <a className="flex items-center text-sm font-medium text-gray-400 hover:text-gray-500">
-                <span>Home</span>
-                <svg
-                  className="ml-6"
-                  width="4"
-                  height="7"
-                  viewBox="0 0 4 7"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M0.150291 0.898704C-0.0500975 0.692525 -0.0500975 0.359292 0.150291 0.154634C0.35068 -0.0507836 0.674443 -0.0523053 0.874831 0.154634L3.7386 3.12787C3.93899 3.33329 3.93899 3.66576 3.7386 3.8727L0.874832 6.84594C0.675191 7.05135 0.35068 7.05135 0.150292 6.84594C-0.0500972 6.63976 -0.0500972 6.30652 0.150292 6.10187L2.49888 3.49914L0.150291 0.898704Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li className="mr-6">
-              <a className="flex items-center text-sm font-medium text-gray-400 hover:text-gray-500">
-                <span>Store</span>
-                <svg
-                  className="ml-6"
-                  width="4"
-                  height="7"
-                  viewBox="0 0 4 7"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M0.150291 0.898704C-0.0500975 0.692525 -0.0500975 0.359292 0.150291 0.154634C0.35068 -0.0507836 0.674443 -0.0523053 0.874831 0.154634L3.7386 3.12787C3.93899 3.33329 3.93899 3.66576 3.7386 3.8727L0.874832 6.84594C0.675191 7.05135 0.35068 7.05135 0.150292 6.84594C-0.0500972 6.63976 -0.0500972 6.30652 0.150292 6.10187L2.49888 3.49914L0.150291 0.898704Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a className="text-sm font-medium text-indigo-500 hover:text-indigo-600">
-                Your cart
-              </a>
-            </li>
-          </ul>
           <div className="pb-9 mb-7 text-center border-b border-black border-opacity-5">
             <h2 className="text-9xl xl:text-10xl leading-normal font-heading font-medium text-center">
               Your cart
@@ -110,7 +67,7 @@ const Cart = () => {
           </div>
           <div className="lg:flex xl:items-center">
             <div className="mb-10 xl:mb-0 lg:w-2/12 xl:w-1/12">
-              <h3 className="text-xl font-heading font-medium">Cart totals</h3>
+              <h3 className="text-xl font-heading font-medium">Cart total</h3>
             </div>
             <div className="sm:flex sm:flex-wrap lg:justify-end lg:w-10/12 xl:w-11/12">
               <div className="sm:pr-3 lg:px-3 mb-6 xl:mb-0 w-full sm:w-1/2 lg:w-4/12 xl:w-3/12">
@@ -123,7 +80,7 @@ const Cart = () => {
                   <span className="ml-16">Products</span>
                 </div>
               </div>
-              <div className="sm:pl-3 lg:px-3 mb-3 xl:mb-0 w-full sm:w-1/2 lg:w-4/12 xl:w-3/12">
+              {/* <div className="sm:pl-3 lg:px-3 mb-3 xl:mb-0 w-full sm:w-1/2 lg:w-4/12 xl:w-3/12">
                 <div className="flex items-center justify-between py-4 px-10 leading-8 bg-white bg-opacity-50 font-heading font-medium rounded-3xl">
                   <span>Shipping</span>
                   <span className="flex items-center">
@@ -131,7 +88,7 @@ const Cart = () => {
                     <span className="text-xl">{formattedShipping}</span>
                   </span>
                 </div>
-              </div>
+              </div> */}
               <div className="sm:pr-3 lg:px-3 mb-10 sm:mb-0 w-full sm:w-1/2 lg:w-4/12 xl:w-3/12">
                 <div className="flex items-center justify-between py-4 px-10 leading-8 bg-white font-heading font-medium rounded-3xl">
                   <span>Total</span>
